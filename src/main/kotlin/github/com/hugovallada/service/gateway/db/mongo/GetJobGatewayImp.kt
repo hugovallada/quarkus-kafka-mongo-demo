99d344b76126f981621575d988134029b15514e7
Hugo Vallada <valladahugo@gmail.com>
2022-09-04 00:43:39 -0300

    Remove código desnecessário

--- src/main/kotlin/github/com/hugovallada/service/gateway/db/mongo/GetJobGatewayImp.kt
+++ src/main/kotlin/github/com/hugovallada/service/gateway/db/mongo/GetJobGatewayImp.kt
@@ -2,23 +2,17 @@ package github.com.hugovallada.service.gateway.db.mongo
 
 import github.com.hugovallada.domain.entity.JobDomain
 import github.com.hugovallada.domain.gateway.GetJobGateway
-import github.com.hugovallada.service.gateway.db.mongo.repository.JobRepository
+import github.com.hugovallada.service.gateway.db.postgres.repository.JobRepository
 import github.com.hugovallada.service.translators.toJobDomain
-import org.bson.types.ObjectId
 import javax.enterprise.context.ApplicationScoped
 
 @ApplicationScoped
-class GetJobGatewayImp(private val jobRepository: JobRepository, private val sqlRepository: github.com.hugovallada.service.gateway.db.postgres.repository.JobRepository): GetJobGateway {
+class GetJobGatewayImp(private val jobRepository: JobRepository): GetJobGateway {
     override fun execute(id: String): JobDomain {
-        println(sqlRepository.listAll())
+        println(jobRepository.listAll())
 
-        sqlRepository.findById(id)?.run {
+        jobRepository.findById(id)?.run {
             return toJobDomain()
-        }
-
-        TODO()
-//        jobRepository.findById(ObjectId(id))?.run {
-//            return toJobDomain()
-//        } ?: throw Exception("")
+        } ?: throw IllegalStateException("Not Found")
     }
 }
